be56747024255d7eea1a5968a7fe6ad9bdd83804
Mathias Brunström <mathiasbrunstrom@gmail.com>
2023-03-31 19:16:59 +0000

    Finished primative types

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,14 +2,18 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = [0;100];
+    let b = [0..10];
+
+    for i in b {
+        println!("{:?}", i);
+    }
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array! {}", a.len());
     } else {
-        println!("Meh, I eat arrays like that for breakfast.");
+        println!("Meh, I eat arrays like that for breakfast.{}", a.len());
     }
 }
